6d08f18b077fdb09979ae1003f5fbd4d3d5b92cd
rajsofan <47587455+rajsofan@users.noreply.github.com>
2024-08-10 21:39:27 +0100

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-     backend "azurerm" {
-       resource_group_name  = "Terraformstate-RG01"
-       storage_account_name = "strgaccountrfrmstate01"
-       container_name       = "tfstate"
-       key                  = "tfstatefile.json"
-     }
-}
