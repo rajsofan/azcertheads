e3e714fbfa723c5cbbd682d693993790585092e6
Rajdeep Das <rajdeep.das@hotmail.com>
2024-08-11 07:23:27 +0100

    updated main.tf

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "azcerheadlabrg" {
+    location = "uksouth"
+    name = "azcertheadlab-rg01"
+  
+}
--- provider.tf
+++ provider.tf
@@ -9,5 +9,6 @@
 		# Configure the Microsoft Azure Provider
 		provider "azurerm" {
 		  features {}
-		  subscription_id = "a14b6fe5-f739-4b09-a63a-99171f6f2984"
+		  subscription_id = "f3b83971-0e0e-4a0b-b17a-a19db7dfd450"
+
 		}
