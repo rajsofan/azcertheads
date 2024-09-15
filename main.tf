523c323d744aaee7bb91f1f739d44053211af67f
Rajdeep Das <rajdeep.das@hotmail.com>
2024-09-15 08:01:29 +0530

    updated

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "azurerm_resource_group" "azcerheadlabrg" {
 resource "azurerm_virtual_network" "azcertheadlabvnet" {
 
   name                = "azvnet_lab01"
-  resource_group_name = azurerm_resource_group.azcertheadlabrg.name
-  location            = azurerm_resource_group.azcertheadlabrg.location
+  resource_group_name = azurerm_resource_group.azcerheadlabrg.name
+  location            = azurerm_resource_group.azcerheadlabrg.location
   address_space       = ["192.168.0.0/24"]
 
 }
@@ -22,7 +22,7 @@ resource "azurerm_virtual_network" "azcertheadlabvnet" {
 
 resource "azurerm_subnet" "azcertheadlabsub1" {
   virtual_network_name = azurerm_virtual_network.azcertheadlabvnet.name
-  resource_group_name  = azurerm_resource_group.azcertheadlabrg.name
+  resource_group_name  = azurerm_resource_group.azcerheadlabrg.name
   name                 = "workloadsubnet"
   address_prefixes     = ["192.168.0.0/26"]
 
@@ -31,19 +31,19 @@ resource "azurerm_subnet" "azcertheadlabsub1" {
 resource "azurerm_subnet" "azcertheadlabgatewaysubnet" {
   name                 = "GatewaySubnet"
   address_prefixes     = ["192.168.0.64/26"]
-  resource_group_name  = azurerm_resource_group.azcertheadlabrg.name
+  resource_group_name  = azurerm_resource_group.azcerheadlabrg.name
   virtual_network_name = azurerm_virtual_network.azcertheadlabvnet.name
 }
 resource "azurerm_subnet" "azcertheadfirewallsubnet" {
   name                 = "AzureFirewallSubnet"
   address_prefixes     = ["192.168.0.128/26"]
-  resource_group_name  = azurerm_resource_group.azcertheadlabrg.name
+  resource_group_name  = azurerm_resource_group.azcerheadlabrg.name
   virtual_network_name = azurerm_virtual_network.azcertheadlabvnet.name
 }
 resource "azurerm_subnet" "azcertheadfirewallmgmtsubnet" {
   name                 = "AzureFirewallManagementSubnet"
   address_prefixes     = ["192.168.0.192/26"]
-  resource_group_name  = azurerm_resource_group.azcertheadlabrg.name
+  resource_group_name  = azurerm_resource_group.azcerheadlabrg.name
   virtual_network_name = azurerm_virtual_network.azcertheadlabvnet.name
 }
 
